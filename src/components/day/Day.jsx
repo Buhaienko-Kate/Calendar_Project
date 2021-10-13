83d97f24000207e7c95c11b046e261fd05bf9232
Buhaienko-Kate <buguzia89@gmail.com>
2021-10-13 18:09:32 +0300

    change day

--- src/components/day/Day.jsx
+++ src/components/day/Day.jsx
@@ -3,13 +3,14 @@ import Hour from '../hour/Hour';
 
 import './day.scss';
 
-const Day = ({ dataDay, dayEvents, deleteEvent }) => {
+const Day = ({ dataDay, dayEvents, deleteEvent, children }) => {
   const hours = Array(24)
     .fill()
     .map((val, index) => index);
 
   return (
     <div className="calendar__day" data-day={dataDay}>
+      {children}
       {hours.map(hour => {
         //getting all events from the day we will render
         const hourEvents = dayEvents.filter(event => new Date(event.dateFrom).getHours() === hour);
